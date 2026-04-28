fac4d6546eb306113700f3e7540e825f5df7357f
Eva Zribi <ezribi1@io86>
2026-04-28 17:44:34 -0400

    make the bash segment_logits.sh command work again: resolve dependencies in requirements.txt to use torch124 compatible with A5000, change the pathways

--- main.py
+++ main.py
@@ -10,33 +10,47 @@ from sklearn.model_selection import train_test_split
 from tqdm import tqdm
 
 from happymap.analysis_results_happymap import (
-    evaluate_metrics_before_after_correction, plot_convergence_algorithm,
-    plot_groupwise_errors_boxplot)
-from happymap.estimate_density import (_estimate_density_upper_bound,
-                                       _estimate_ordered_density_upper_bound)
+    evaluate_metrics_before_after_correction,
+    plot_convergence_algorithm,
+)
 from happymap.fair_segmentation_boost import FairSegmentationBoost
 from happymap.metrics import get_error_function
-from happymap.subgroup_metrics_for_image_segmentation_version_with_value import \
-    subgroup_metrics
+
+# from happymap.subgroup_metrics_for_image_segmentation_version_with_value import \
+#     subgroup_metrics
+from happymap.subgroup_metrics_for_image_segmentation import subgroup_metrics
 from preprocess.preprocess_results import build_data_for_organ
 
-if __name__=="__main__":
+if __name__ == "__main__":
     # Preprocess and build the data for stomach without saving (30mn)
-    path_total_segmentator = "/export/gaon1/data/jteneggi/TotalSegmentator"
-    path_results = f"/cis/home/ezribi1/my_documents/fair_segmentation/results"
-    path_data_organ = f"/cis/home/ezribi1/my_documents/fair_segmentation/data_organs"
-    
+    # path_total_segmentator = "/export/gaon1/data/jteneggi/TotalSegmentator"
+    path_total_segmentator = "/export/io86/data/jteneggi/TotalSegmentator"
+    # path_results = "/cis/home/ezribi1/my_documents/fair_segmentation/results"
+    path_results = "/cis/home/ezribi1/my_documents/fairness_image_segmentation_clean/results"  # only 10 images for now, just to check that the pipeline works
+    # path_data_organ = "/cis/home/ezribi1/my_documents/fair_segmentation/data_organs"
+    path_data_organ = (
+        "/cis/home/ezribi1/my_documents/fairness_image_segmentation_clean/data_organs"
+    )
+
     print("Building data for stomach...")
     dict_stomach = build_data_for_organ(
+        # organ="stomach",
         organ="stomach",
         path_results=path_results,
         path_total_segmentator=path_total_segmentator,
         group_columns=["age", "gender"],
         save_path=None,
-        max_samples=100  # Limit to 128 samples for faster testing
+        max_samples=100,  # Limit to 128 samples for faster testing
     )
-    f_all, h_all, y_all, g_all, sample_ids, group_names = dict_stomach["f"], dict_stomach["h"], dict_stomach["y"], dict_stomach["g"], dict_stomach["sample_ids"], dict_stomach["group_names"]
-    
+    f_all, h_all, y_all, g_all, sample_ids, group_names = (
+        dict_stomach["f"],
+        dict_stomach["h"],
+        dict_stomach["y"],
+        dict_stomach["g"],
+        dict_stomach["sample_ids"],
+        dict_stomach["group_names"],
+    )
+
     # Take a small portion of the data for faster testing
     # n_samples_total = len(f_all)
     # n_samples_subset = min(128, n_samples_total)  # Use at most 200 samples for testing
@@ -47,17 +61,44 @@ if __name__=="__main__":
 
     # Split into train, val, test (60%, 20%, 20%)
     print("\nSplitting data into train, val, test...")
-    idx_train, idx_temp = train_test_split(np.arange(len(f_all)), test_size=0.4, random_state=42)
+    idx_train, idx_temp = train_test_split(
+        np.arange(len(f_all)), test_size=0.4, random_state=42
+    )
     idx_val, idx_test = train_test_split(idx_temp, test_size=0.5, random_state=42)
-    print("Train size:", len(idx_train), "\tVal size:", len(idx_val), "\tTest size:", len(idx_test))
+    print(
+        "Train size:",
+        len(idx_train),
+        "\tVal size:",
+        len(idx_val),
+        "\tTest size:",
+        len(idx_test),
+    )
 
-    f_train, h_train, y_train, groups_train, sample_ids_train = f_all[idx_train], h_all[idx_train], y_all[idx_train], g_all[idx_train], sample_ids[idx_train]
-    f_val, h_val, y_val, groups_val, sample_ids_val = f_all[idx_val], h_all[idx_val], y_all[idx_val], g_all[idx_val], sample_ids[idx_val]
-    f_test, h_test, y_test, groups_test, sample_ids_test = f_all[idx_test], h_all[idx_test], y_all[idx_test], g_all[idx_test], sample_ids[idx_test]
+    f_train, h_train, y_train, groups_train, sample_ids_train = (
+        f_all[idx_train],
+        h_all[idx_train],
+        y_all[idx_train],
+        g_all[idx_train],
+        sample_ids[idx_train],
+    )
+    f_val, h_val, y_val, groups_val, sample_ids_val = (
+        f_all[idx_val],
+        h_all[idx_val],
+        y_all[idx_val],
+        g_all[idx_val],
+        sample_ids[idx_val],
+    )
+    f_test, h_test, y_test, groups_test, sample_ids_test = (
+        f_all[idx_test],
+        h_all[idx_test],
+        y_all[idx_test],
+        g_all[idx_test],
+        sample_ids[idx_test],
+    )
 
     # Initialize and fit the algorithm
     print("\nFitting FairSegmentationBoost...")
-    fsb = FairSegmentationBoost(metric_name = "IoU", step_size=0.1)
+    fsb = FairSegmentationBoost(metric_name="IoU", step_size=0.1)
     alpha = 0.3
     param_fsb = f"(alpha={alpha}, step_size={fsb.step_size},n={len(f_all)})"
 
@@ -80,24 +121,35 @@ if __name__=="__main__":
     # Evaluate on the test set BEFORE and AFTER correction
     print("\nAnalysis of results")
     print("\nEvaluating metrics before and after correction on test set...")
-    evaluate_metrics_before_after_correction(groups_test, y_test, h_test, f_test, fsb, save_path=f"./output/metrics_before_after_stomach_{param_fsb}.pkl")
+    evaluate_metrics_before_after_correction(
+        groups_test,
+        y_test,
+        h_test,
+        f_test,
+        fsb,
+        save_path=f"./output/metrics_before_after_stomach_{param_fsb}.pkl",
+    )
 
     # Plot convergence of the algorithm
     print("\nPlotting convergence of the algorithm...")
-    plot_convergence_algorithm(fsb, save_path=f"./output/convergence_plot_stomach_{param_fsb}.png", name_task="Stomach")
+    plot_convergence_algorithm(
+        fsb,
+        save_path=f"./output/convergence_plot_stomach_{param_fsb}.png",
+        name_task="Stomach",
+    )
 
     # Generate and plot boxplots by groups
     print("\nGenerating and plotting boxplots by groups...")
     f_val_round = np.round(f_val * fsb.m) / fsb.m
     f_val_corrected = fsb.predict(f_val, groups_val)
 
-    plot_groupwise_errors_boxplot(
-        f_val_round,
-        f_val_corrected,
-        h_val,
-        y_val,
-        groups_val,
-        group_names=group_names,#['age1', 'age2', 'age3', 'age4', 'age5', 'f', 'm'],
-        metric_name="IoU",
-        save_path=f"./output/boxplot_fairness_by_group_stomach_{param_fsb}.png"
-    )
+    # plot_groupwise_errors_boxplot(
+    #     f_val_round,
+    #     f_val_corrected,
+    #     h_val,
+    #     y_val,
+    #     groups_val,
+    #     group_names=group_names,#['age1', 'age2', 'age3', 'age4', 'age5', 'f', 'm'],
+    #     metric_name="IoU",
+    #     save_path=f"./output/boxplot_fairness_by_group_stomach_{param_fsb}.png"
+    # )
--- preprocess/preprocess_results.py
+++ preprocess/preprocess_results.py
@@ -5,10 +5,11 @@ import nibabel as nib
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
+from typing import List, Dict
 
 
 def load_groups_from_csv(
-    csv_path: str, group_columns: list[str], continuous_bins: dict = {"age": 5}
+    csv_path: str, group_columns: List[str], continuous_bins: Dict = {"age": 5}
 ):
     """ "
         Load group indicators from a CSV file.
@@ -68,6 +69,7 @@ def load_predictions_and_labels(
     """
     print("Loading predictions and labels (f,h,y, sample_ids) for organ:", organ)
     sample_ids = sorted([f for f in os.listdir(path_results) if f.startswith("s")])
+    print("sample_ids.shape:", len(sample_ids)) # 1159
 
     if max_samples is not None:
         n_samples_total = len(sample_ids)
@@ -85,7 +87,11 @@ def load_predictions_and_labels(
             path_total_segmentator, sid, "segmentations", f"{organ}.nii.gz"
         )
 
-        if not os.path.exists(h_path) or not os.path.exists(y_path):
+        if not os.path.exists(h_path):
+            print("Missing files for sample_id: ", sid, "h_path:", h_path)
+            continue
+        if  not os.path.exists(y_path):
+            print("Missing files for sample_id: ", sid, "y_path:", y_path)
             continue
 
         # Here: will have to choose best slice (smaller vectors)
@@ -95,10 +101,12 @@ def load_predictions_and_labels(
         h_list.append(h_data)
         y_list.append(y_data)
         filtered_sample_ids.append(sid)
+        # print("loop of load_predictions_and_labels")
 
     h = np.array(h_list, dtype=object)
     y = np.array(y_list, dtype=object)
     f = np.full((h.shape[0],), 0.5)
+    print("After loop load_predicitons, h.shape:", h.shape, "y.shape:", y.shape, "f.shape:", f.shape, "filtered_sample_ids.shape:", len(filtered_sample_ids)) # 1159
 
     return f, h, y, filtered_sample_ids
 
@@ -132,9 +140,11 @@ def build_data_for_organ(
     f, h, y, sample_ids = load_predictions_and_labels(
         path_results, path_total_segmentator, organ, max_samples=max_samples
     )
+    print("f.shape:", f.shape, "h.shape:", h.shape, "y.shape:", y.shape, "sample_ids.shape:", len(sample_ids)) # 0 : bug comes from here
     g, g_ids, group_names = load_groups_from_csv(
         path_total_segmentator + "/meta.csv", group_columns
     )
+    print("g.shape:", g.shape) #  (1226, 7)
 
     id_to_index = {sid: i for i, sid in enumerate(sample_ids)}
     common_ids = [sid for sid in g_ids if sid in id_to_index]
